169ce045ef06029a9eb73c7d5572bbfaa1946127
adeola <adeola.akinsanya@gmail.com>
2024-04-17 14:01:13 -0400

    created scratch folder in docker

--- apps/utils/sflc_fileops.py
+++ apps/utils/sflc_fileops.py
@@ -66,7 +66,7 @@ def convert_audit_trail_to_json_files(data: list) -> None:
     file_ext = 0  # Required to make each file unique.
     for record in data:
         file_ext += 1
-        with open(f"Audit_Trail.{file_ext}.json", "w") as file:
+        with open(f"scratch/Audit_Trail.{file_ext}", "w") as file:
             json.dump(record, file)
 
 
@@ -74,21 +74,21 @@ def convert_csv_file_to_json_files(csv_file_name: str, downloaded_file: bytes) -
     # Each entry in the CSV Log file downloaded from salesforce is converted to a json file
 
     # Write raw data in CSV format to CSV file.
-    with open(csv_file_name, "wb") as file:
+    with open(f"scratch/{csv_file_name}", "wb") as file:
         file.write(downloaded_file)
-    csv_file = pd.read_csv(csv_file_name)
+    csv_file = pd.read_csv(f"scratch/{csv_file_name}")
 
     # Convert csv file to python object.
     records = csv_file.to_dict(orient="records")
 
     # Delete CSV file.
-    Path(csv_file_name).unlink()
+    Path(f"scratch/{csv_file_name}").unlink()
 
     # Write each event to a JSON file.
     file_ext = 0  # Required to make each file unique
     for record in records:
         file_ext += 1
-        with open(f"{csv_file_name}.{file_ext}.json", "w") as file:
+        with open(f"scratch/{csv_file_name}.{file_ext}", "w") as file:
             json.dump(record, file)
 
 
@@ -102,8 +102,8 @@ def upload_to_storage_bucket(google_project_id: str, storage_bucket: str) -> Non
 def write_to_bucket_blobs(storage_bucket: Bucket) -> None:
     # Generated JSON files are uploaded to a Cloud Storage folder
     # Filename format in Cloud Storage will be 'YY-mm-ddTHH:MM:SSZ-logfile.json'
-    current_working_dir = Path.cwd()
-    json_files = current_working_dir.glob("*.json")
+    scratch_dir = Path.cwd() / "scratch"
+    json_files = scratch_dir.glob("*")
     date_time_format = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
     record_num = 1
 
